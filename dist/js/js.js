60946bdd6f7f4ecc4dbf54b2f48b777b730552c8
suwimol <suwi,ol.u@gmail.com>
2019-01-20 11:22:13 +0700

    animation and dropdown

---
+++ dist/js/js.js
@@ -0,0 +1,9 @@
+
+
+function showDiv(){
+    if(document.getElementById('showDiv').style.display == 'none'){
+        document.getElementById('showDiv').style.display = 'block';
+    }else{
+        document.getElementById('showDiv').style.display = 'none';
+    }
+}
